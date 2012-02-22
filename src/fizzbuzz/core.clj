6d97ba998938be8777682871b2dd03f6e4a0d11a
Jean Perrot <Jean.brahnperrot@gmail.com>
2012-02-22 11:40:27 -0800

    more fizzbuzz

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -2,8 +2,23 @@
 
 (defn fizzbuzz []
   (dotimes [n 100]
-    (cond (zero? (rem n 15)) (prn "fizzbuzz")
-          (zero? (rem n 3)) (prn "fizz")
-          (zero? (rem n 5)) (prn "buzz")
-          :else (prn n)
-          )))
+    (let [n (inc n)]
+      (cond (zero? (rem n 15)) (prn "fizzbuzz")
+            (zero? (rem n 3)) (prn "fizz")
+            (zero? (rem n 5)) (prn "buzz")
+            :else (prn n)
+            ))))
+
+
+(defn fizzbuzz-2[]
+  (defn divisible? [n x] (zero? (rem n x)))
+  (defn mapTo [n]
+    (cond (divisible? n 15) "fizzbuzz"
+          (divisible? n 3) "fizz"
+          (divisible? n 5) "buzz"
+          :else n
+          ))
+  (dotimes [n 100]
+    (let [n (inc n)]
+      (prn (mapTo n)))))
+          
