5d3d84ac29ed39a070cae028e516b9a133e9686f
Jean Perrot <Jean.brahnperrot@gmail.com>
2012-02-22 22:00:45 -0800

    more versions

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -21,4 +21,35 @@
   (dotimes [n 100]
     (let [n (inc n)]
       (prn (mapTo n)))))
-          
+ 
+(defn fizzbuzz-3[]
+  (defn special [n]
+    (defn divisible? [x] (zero? (rem n x)))
+    (str (when (divisible? 3) "fizz")
+         (when (divisible? 5) "buzz")))
+  (defn mapTo[n]
+    (let [spec (special n)]
+      (if (empty? spec) n
+        spec)))
+  (dotimes [n 100]
+    (let [n (inc n)]
+      (prn (mapTo n)))))
+
+
+(def par {3 "fizz" 5 "buzz"})
+
+(defn fizzbuzz-4[]
+  (defn special [n]
+    (defn divisible? [x] (zero? (rem n x)))
+    (defn mapSingle [entry]
+      (if (divisible? (entry 0)) (entry 1)))
+    (apply str (map mapSingle par)))
+  (defn mapTo[n]
+    (let [spec (special n)]
+      (if (empty? spec) n
+        spec)))
+  (dotimes [n 100]
+    (let [n (inc n)]
+      (prn (mapTo n)))))
+
+
